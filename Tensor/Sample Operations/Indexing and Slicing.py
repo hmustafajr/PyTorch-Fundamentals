0a56cd165eb7cbfecb7fd072f01af7daa5b75822
Hassan (mufasa) <hmustafajr@gmail.com>
2025-02-06 19:35:53 -0800

    Update Indexing and Slicing.py

--- Tensor/Sample Operations/Indexing and Slicing.py
+++ Tensor/Sample Operations/Indexing and Slicing.py
@@ -4,3 +4,12 @@ print(f"First Column: {tensor[:, 0]}")
 print(f"Last Column: {tensor[..., -1]}")
 tensor[:,1] = 0
 print(tensor)
+
+# Output example
+#    First row: tensor([1., 1., 1., 1.])
+#    First column: tensor([1., 1., 1., 1.])
+#    Last column: tensor([1., 1., 1., 1.])
+#    tensor([[1., 0., 1., 1.],
+#           [1., 0., 1., 1.],
+#           [1., 0., 1., 1.],
+#           [1., 0., 1., 1.]])
