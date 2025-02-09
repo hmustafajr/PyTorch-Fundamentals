fb8b56aa60628a329c68a63f7593ddacba6da966
Hassan (mufasa) <hmustafajr@gmail.com>
2025-02-08 17:25:41 -0800

    Update In Place Operations.py

--- Tensor/Sample Operations/In Place Operations.py
+++ Tensor/Sample Operations/In Place Operations.py
@@ -1,4 +1,7 @@
-# In place operations store the results into the operand. These are denoted by a _ suffix.
+# In-place operations store the results into the operand. These are denoted by a _ suffix.
 print(f" {tensor} \n")
 tensor.add_(5)
 print(tensor)
+
+# In-place operations save some memory, but can be problematic when computing derivatives
+# because of the immediate loss of history.
