53af2cb1ba1fd98cdfb35b3686c274ade64259bb
Hassan (mufasa) <hmustafajr@gmail.com>
2025-02-08 17:57:36 -0800

    Update Bridging.py

--- Bridging.py
+++ Bridging.py
@@ -1,4 +1,4 @@
-# Tensors on the CPU and NumPy arrays can share memory locations and changing one will change the other
+# tensors on the CPU and NumPy arrays can share memory locations and changing one will change the other
 
 # Tensor to NumPy array
 t = torch.ones(5)
@@ -8,3 +8,11 @@ print(f"n: {n}")
 # Output
 #   t: tensor([1., 1., 1., 1., 1.])
 #   n: [1. 1. 1. 1. 1.]
+
+# a change to the tensor reflects in the NumPy array
+t.add(1)
+print(f"t: {t}")
+print(f"n: {n}")
+# Output
+#   t: tensor([2., 2., 2., 2., 2.])
+#   n: [2. 2. 2. 2. 2.]
