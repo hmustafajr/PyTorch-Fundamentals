7dc6677ae0fe81477f9b5760359e8fe6cd670ab1
Hassan (mufasa) <hmustafajr@gmail.com>
2025-03-02 09:48:00 -0800

    Create Optimization.py

---
+++ Building Neural Networks/Optimization.py
@@ -0,0 +1,68 @@
+# Now that we have a model and data it's time to train, validate and test our model by optimizing its parameters
+# on our data. Training a model is an iterative process, in each iteration the model makes a guess about the output
+# calculates the error in its guess(loss), collects the derivatives of the error with respect to it's parameters
+# and optimizes these parameters using gradient descent.
+
+# Prerequisite Code
+# We'll load code from the Datasets & DataLoaders and Buld Model
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transform import ToTensor
+
+training_data = datasets.FashionMNIST(
+  root = "data",
+  train = true,
+  download = true,
+  transform = ToTensor()
+)
+
+train_dataloader = DateLoader(training_data, batch_size = 64)
+test_dataloader = DataLoader(test_data, batch_size = 64)
+
+class NeuralNetwork(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.flatten = nn.Flatten()
+    self.linear_relu_stack = nn.Sequential(
+      nn.linear(28 * 28, 512),
+      nn.ReLU(),
+      nn.Linear(512, 512),
+      nn.ReLU(),
+      nn.Linear(512, 10),
+    )
+def forward(self, x):
+  x = self.flatten(x)
+  logits = self.linear_relu_stack(x)
+  return logits
+model = NeuralNetwork()
+
+# Out
+#   0%|          | 0.00/26.4M [00:00<?, ?B/s]
+#  0%|          | 65.5k/26.4M [00:00<01:12, 361kB/s]
+#  1%|          | 229k/26.4M [00:00<00:38, 679kB/s]
+#  3%|3         | 885k/26.4M [00:00<00:10, 2.52MB/s]
+#  7%|7         | 1.93M/26.4M [00:00<00:05, 4.09MB/s]
+# 23%|##3       | 6.19M/26.4M [00:00<00:01, 14.3MB/s]
+# 38%|###7      | 9.93M/26.4M [00:00<00:00, 17.3MB/s]
+# 57%|#####7    | 15.1M/26.4M [00:01<00:00, 25.7MB/s]
+# 71%|#######1  | 18.9M/26.4M [00:01<00:00, 24.2MB/s]
+# 91%|#########1| 24.1M/26.4M [00:01<00:00, 30.7MB/s]
+# 100%|##########| 26.4M/26.4M [00:01<00:00, 19.3MB/s]
+
+#  0%|          | 0.00/29.5k [00:00<?, ?B/s]
+# 100%|##########| 29.5k/29.5k [00:00<00:00, 324kB/s]
+
+#  0%|          | 0.00/4.42M [00:00<?, ?B/s]
+#  1%|1         | 65.5k/4.42M [00:00<00:12, 361kB/s]
+#  4%|4         | 197k/4.42M [00:00<00:05, 767kB/s]
+# 10%|9         | 426k/4.42M [00:00<00:03, 1.07MB/s]
+# 36%|###6      | 1.61M/4.42M [00:00<00:00, 4.26MB/s]
+# 79%|#######8  | 3.47M/4.42M [00:00<00:00, 7.09MB/s]
+# 100%|##########| 4.42M/4.42M [00:00<00:00, 6.05MB/s]
+
+#  0%|          | 0.00/5.15k [00:00<?, ?B/s]
+# 100%|##########| 5.15k/5.15k [00:00<00:00, 40.5MB/s]
+
