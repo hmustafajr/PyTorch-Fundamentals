1140a2b041e1a3972c296d398ef94133ae1ec016
Hassan (mufasa) <hmustafajr@gmail.com>
2025-03-14 18:43:50 -0700

    Update PyTorch Tensors.py

--- Introduction/PyTorch Tensors.py
+++ Introduction/PyTorch Tensors.py
@@ -23,3 +23,17 @@ tensor([[1, 1, 1],
         [1, 1, 1],
         [1, 1, 1],
         [1, 1, 1]], dtype=torch.int16) # Observe that this is now an integer
+#
+# It's common to initialize learning weights randomly, often with a specific seed for the PRNG for reproducibility
+torch.manual_seed(1729)
+r1 = torch.ran(2, 2)
+print('Random Tensor:')
+print(r1)
+
+r2 = torch.rand_seed(2, 2)
+print('Another Random Tensor')
+print(r2)
+
+torch.manual_seed(1729)
+print('\nShould match r1:')
+print(r3) # repeats values of r1 because of re-seed
